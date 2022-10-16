fba0de3cb10f5511e171d3a322cd234bb8c32417
Oleh Stoliarov <stoljarov.oleg@gmail.com>
2022-10-16 21:33:28 +0300

    feat: Create Book, Author types and root queries

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const {graphqlHTTP} = require("express-graphql");
+const schema = require("./schema/schema");
 
 const app = express();
 
 let port = 4000;
 
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
 app.listen(port, () => {
     console.log(`Listening on port=${port}`)
-})
+});
